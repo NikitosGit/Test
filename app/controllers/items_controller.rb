8d84abbacf222d9289082b671d19968ca68cb471
Nos <nikitos_05@mail.ru>
2014-11-01 17:58:00 +0800

    before_filter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,69 +1,77 @@
 class ItemsController < ApplicationController
 	
-def index	
-	@items = Item.all
-	render "items/index" # рендер можно не указывать. 
-						 #По умолчанию рендерится шаблон, совпадающий с названием метода т. е. index.html.erb
-	
-	#render text: @items.map {|i| "#{i.id} #{i.name}: #{i.price}"}.join("<br/>")
-end
+	before_filter :find_item, only: [:show, :edit, :update, :destroy]
+	before_filter :check_if_admin,  only: [:edit, :update, :new, :create, :destroy]
 
-#url /items/1 GET
-def show
-	unless @item = Item.where(id: params[:id]).first #@item = Item.find(params[:id]) - сам придумал :) вроде работает
-		render text: "Page not found", status: 404
+	def index	
+		@items = Item.all
+		render "items/index" # рендер можно не указывать. 
+							 #По умолчанию рендерится шаблон, совпадающий с названием метода т. е. index.html.erb
+		
+		#render text: @items.map {|i| "#{i.id} #{i.name}: #{i.price}"}.join("<br/>")
 	end
-end
 
-#url /items/new GET
-def new
-	@item = Item.new
-end
+	#url /items/1 GET
+	def show
+		#unless @item = Item.where(id: params[:id]).first #@item = Item.find(params[:id]) - сам придумал :) вроде работает
+		unless @item
+			render text: "Page not found", status: 404
+		end
+	end
 
-#url /items/1/edit GET
-def edit
-	@item = Item.find(params[:id])
-end
+	#url /items/new GET
+	def new
+		@item = Item.new
+	end
 
-#url /items POST
-def create
-	#@item = Item.create(name: params[:name], description: params[:description], price: 
-	#	params[:price], real: params[:real], weight: params[:weight])
-	
-	@item = Item.create(item_params)
-	if @item.errors.empty?
-		redirect_to item_path(@item)	#items/:id
-	else
-		render "new"
-	end	
-#	p params
-#	render text: "#{@item.id}: #{@item.name} #{@item.price} (#{!@item.new_record?})"
+	#url /items/1/edit GET
+	def edit
+	end
 
-#	render text: params.inspect
-end
+	#url /items POST
+	def create
+		#@item = Item.create(name: params[:name], description: params[:description], price: 
+		#	params[:price], real: params[:real], weight: params[:weight])
+		
+		@item = Item.create(item_params)
+		if @item.errors.empty?
+			redirect_to item_path(@item)	#items/:id
+		else
+			render "new"
+		end	
+	#	p params
+	#	render text: "#{@item.id}: #{@item.name} #{@item.price} (#{!@item.new_record?})"
 
-#url /items/1 PUT
-def update
-	@item = Item.find(params[:id])
-	@item.update_attributes(item_params)#item_params - хэш параметров из браузера
-	if @item.errors.empty?
-		redirect_to item_path(@item)	#items/:id
-	else
-		render "edit"
-	end	
-end
+	#	render text: params.inspect
+	end
 
-#url /items/1 DELETE
-def destroy
-	@item = Item.find(params[:id])
-	@item.destroy
-	redirect_to action: "index"
-end
+	#url /items/1 PUT
+	def update
+		@item.update_attributes(item_params)#item_params - хэш параметров из браузера
+		if @item.errors.empty?
+			redirect_to item_path(@item)	#items/:id
+		else
+			render "edit"
+		end	
+	end
 
-private 
-	
-def item_params
-	params.require(:item).permit(:name, :description, :price, :weight, :real)
-end
+	#url /items/1 DELETE
+	def destroy
+		@item.destroy
+		redirect_to action: "index"
+	end
 
+	private 
+		
+	def item_params
+		params.require(:item).permit(:name, :description, :price, :weight, :real)
+	end
+
+	def find_item
+		@item = Item.find(params[:id])
+	end
+
+	def check_if_admin
+		render text: "Access denied", status: 403 unless params[:admin]
+	end
 end
